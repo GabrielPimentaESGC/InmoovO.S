111f7f57b0c61499892c9b331f4513af6bac3ddf
GabrielPimentaESGC <gabriel.pimenta.30977@esgc.pt>
2023-07-02 02:20:01 +0100

    Update InmoovO.S .py

--- InmoovO.S .py
+++ InmoovO.S .py
@@ -1,35 +1,56 @@
 import pygame
+import os
 
 # Inicializa o Pygame
 pygame.init()
 
-# Define a resolução da janela
-resolucao_tela = (800, 600)  # Substitua pelos valores desejados
+# Obtém informações sobre a tela atual
+info_tela = pygame.display.Info()
 
-# Cria a janela
-janela = pygame.display.set_mode(resolucao_tela)
+# Obtém a resolução da tela
+resolucao_tela = (info_tela.current_w, info_tela.current_h)
 
-# Define as cores
-COR_BG = (255, 255, 255)  # Branco
-COR_BOTAO = (0, 0, 255)  # Azul
+# Cria a janela fullscreen com a resolução da tela
+janela = pygame.display.set_mode(resolucao_tela, pygame.FULLSCREEN)
 
-# Define as dimensões e margens dos botões
+# Carrega a imagem de background
+caminho_imagem = '"C:\\Users\\Gabriel Pimenta\\Desktop\\ReposGitHub\\InmoovO.S\\app\\assets\\background_default.png"'  
+
+imagem_background = pygame.transform.scale(imagem_background, resolucao_tela)
+
+# Carrega os sprites dos botões
+caminho_botao_pergunta = '"C:\\Users\\Gabriel Pimenta\\Desktop\\ReposGitHub\\InmoovO.S\\app\\assets\\buttons\\pt\\botao_pergunta.png"'  
+caminho_botao_musica = '"C:\\Users\\Gabriel Pimenta\\Desktop\\ReposGitHub\\InmoovO.S\\app\\assets\\buttons\\pt\\botao_musica.png"'  
+caminho_botao_factos = 'caminho\\para\\o\\sprite_factos.png'  # Substitua pelo caminho do sprite do botão factos com \\
+caminho_botao_servos = 'caminho\\para\\o\\sprite_servos.png'  # Substitua pelo caminho do sprite do botão servos com \\
+caminho_botao_settings = 'caminho\\para\\o\\sprite_settings.png'  # Substitua pelo caminho do sprite do botão settings com \\
+
+# Define as dimensões dos botões
 largura_botao = 100
 altura_botao = 50
-margem_horizontal = 20
-margem_vertical = 10
 
-# Calcula as posições dos botões
-num_botoes = 5
-total_botoes = 3 + num_botoes
-espaco_vertical = (resolucao_tela[1] - (total_botoes * altura_botao) - ((total_botoes - 1) * margem_vertical)) // 2
-posicoes_botao = []
-for i in range(num_botoes):
-    x = (resolucao_tela[0] - largura_botao) // 2
-    y = espaco_vertical + (i // 2) * (altura_botao + margem_vertical)
-    if i % 2 == 1:
-        x += largura_botao + margem_horizontal
-    posicoes_botao.append((x, y))
+# Define as posições dos botões
+posicao_x = (resolucao_tela[0] - (largura_botao * 3)) / 2
+posicao_y = resolucao_tela[1] / 2
+
+# Carrega os sprites dos botões redimensionados
+botao_pergunta = pygame.image.load(caminho_botao_pergunta)
+botao_pergunta = pygame.transform.scale(botao_pergunta, (largura_botao, altura_botao))
+
+botao_musica = pygame.image.load(caminho_botao_musica)
+botao_musica = pygame.transform.scale(botao_musica, (largura_botao, altura_botao))
+
+botao_factos = pygame.image.load(caminho_botao_factos)
+botao_factos = pygame.transform.scale(botao_factos, (largura_botao, altura_botao))
+
+botao_servos = pygame.image.load(caminho_botao_servos)
+botao_servos = pygame.transform.scale(botao_servos, (largura_botao, altura_botao))
+
+botao_settings = pygame.image.load(caminho_botao_settings)
+botao_settings = pygame.transform.scale(botao_settings, (largura_botao, altura_botao))
+
+# Define o título da janela
+pygame.display.set_caption("Sistema Operativo")
 
 # Loop principal do programa
 executando = True
@@ -39,26 +60,34 @@ while executando:
         if evento.type == pygame.QUIT:
             executando = False
 
-    # Obtém a posição do mouse
-    pos_mouse = pygame.mouse.get_pos()
+    # Desenha a imagem de background na janela
+    janela.blit(imagem_background, (0, 0))
 
-    # Limpa a tela
-    janela.fill(COR_BG)
+    # Obtém a posição do cursor do mouse
+    posicao_mouse = pygame.mouse.get_pos()
 
-    # Desenha os botões
-    for posicao in posicoes_botao:
-        # Define a área do botão
-        botao_rect = pygame.Rect(posicao[0], posicao[1], largura_botao, altura_botao)
-
-        # Verifica se o mouse está sobre o botão
-        if botao_rect.collidepoint(pos_mouse):
-            # Diminui o tamanho do botão em 5%
-            botao_diminuido = botao_rect.inflate(-largura_botao * 0.05, -altura_botao * 0.05)
-            pygame.draw.rect(janela, COR_BOTAO, botao_diminuido)
+    # Verifica se o cursor do mouse está sobre o botão factos e settings
+    if 1 in [adm]:  # Substitua pelo valor da sua variável 'adm'
+        if posicao_x <= posicao_mouse[0] <= posicao_x + largura_botao and posicao_y <= posicao_mouse[1] <= posicao_y + altura_botao:
+            botao_factos_diminuido = pygame.transform.scale(botao_factos, (int(largura_botao * 0.95), int(altura_botao * 0.95)))
         else:
-            pygame.draw.rect(janela, COR_BOTAO, botao_rect)
+            botao_factos_diminuido = botao_factos
 
-    # Atualiza a tela
+        if posicao_x + largura_botao <= posicao_mouse[0] <= posicao_x + largura_botao * 2 and posicao_y <= posicao_mouse[1] <= posicao_y + altura_botao:
+            botao_settings_diminuido = pygame.transform.scale(botao_settings, (int(largura_botao * 0.95), int(altura_botao * 0.95)))
+        else:
+            botao_settings_diminuido = botao_settings
+    else:
+        botao_factos_diminuido = botao_factos
+        botao_settings_diminuido = botao_settings
+
+    # Desenha os botões na janela
+    janela.blit(botao_pergunta, (posicao_x, posicao_y))
+    janela.blit(botao_musica, (posicao_x + largura_botao, posicao_y))
+    janela.blit(botao_factos_diminuido, (posicao_x + largura_botao * 2, posicao_y))
+    janela.blit(botao_settings_diminuido, (posicao_x + largura_botao * 3, posicao_y))
+
+    # Atualiza a janela
     pygame.display.flip()
 
 # Encerra o Pygame
