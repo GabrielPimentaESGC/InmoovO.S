6cf4fc842026e61d8f447e8fb3d02c264ce05993
GabrielPimentaESGC <gabriel.pimenta.30977@esgc.pt>
2023-07-10 14:38:50 +0100

    logo

--- app/InmoovO.S .py
+++ app/InmoovO.S .py
@@ -1,129 +1,84 @@
 import pygame
-import requests
-from io import BytesIO
+import urllib.request
+import io
 
-# Initialize Pygame
+# Configurações
+BACKGROUND_URL = "https://github.com/GabrielPimentaESGC/InmoovO.S/blob/main/app/assets/background_default.png?raw=true"
+BUTTON_NORMAL_URL = "https://github.com/GabrielPimentaESGC/InmoovO.S/blob/main/app/assets/buttons/pt/botao_pergunta.png?raw=true"
+BUTTON_HOVER_SCALE = 0.85
+BUTTON_PRESSED_SCALE = 1.15
+
+# Inicialização do Pygame
 pygame.init()
 
-# Get the screen size
+# Obtém a resolução do monitor
 screen_info = pygame.display.Info()
 screen_width = screen_info.current_w
 screen_height = screen_info.current_h
 
-# Set up the display
-screen = pygame.display.set_mode((screen_width, screen_height), pygame.FULLSCREEN)
+# Carrega as imagens do background e do botão
+background_image_data = urllib.request.urlopen(BACKGROUND_URL).read()
+background_image = pygame.image.load(io.BytesIO(background_image_data))
+button_normal_image_data = urllib.request.urlopen(BUTTON_NORMAL_URL).read()
+button_normal_image = pygame.image.load(io.BytesIO(button_normal_image_data))
 
-# Download the image from the URL
-image_url = "https://github.com/GabrielPimentaESGC/InmoovO.S/blob/main/app/assets/background_default.png?raw=true"
-response = requests.get(image_url)
-image_data = response.content
+# Define as escalas inicial e atual do botão
+button_scale = 1.0
+button_current_scale = 1.0
 
-# Load the image from the downloaded data
-image = pygame.image.load(BytesIO(image_data))
+# Função para desenhar o botão centralizado na tela
+def draw_button():
+    button_rect = button_normal_image.get_rect()
+    button_rect.center = (screen_width // 2, screen_height // 2)
+    button_scaled_width = int(button_rect.width * button_current_scale)
+    button_scaled_height = int(button_rect.height * button_current_scale)
+    button_image_scaled = pygame.transform.scale(button_normal_image, (button_scaled_width, button_scaled_height))
+    screen.blit(button_image_scaled, button_rect)
 
-# Scale the image to fit the screen
-image = pygame.transform.scale(image, (screen_width, screen_height))
+# Cria a janela em tela cheia
+screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 
-# Download the button image from the URL
-button_url = "https://github.com/GabrielPimentaESGC/InmoovO.S/blob/main/app/assets/buttons/pt/botao_pergunta.png?raw=true"
-response = requests.get(button_url)
-button_data = response.content
-
-# Load the button image from the downloaded data
-button_image = pygame.image.load(BytesIO(button_data))
-button_width = int(screen_width * 0.15)
-button_height = int(screen_height * 0.15)
-button_image = pygame.transform.scale(button_image, (button_width, button_height))
-
-# Define the initial and scaled sizes for the button
-button_scale_normal = (button_width, button_height)
-button_scale_hover = (int(button_width * 0.85), int(button_height * 0.85))
-button_scale_pressed = (int(button_width * 1.2), int(button_height * 1.2))
-
-# Set the initial button position
-button_pos = (int(screen_width * 0.5 - button_width * 0.5), int(screen_height * 0.5 - button_height * 0.5))
-
-# Define chatbox parameters
-chatbox_width = int(screen_width * 0.6)
-chatbox_height = int(screen_height * 0.6)
-chatbox_pos = (int(screen_width * 0.5 - chatbox_width * 0.5), int(screen_height * 0.5 - chatbox_height * 0.5))
-chatbox_color = (255, 255, 255)
-chatbox_border_color = (0, 0, 0)
-chatbox_border_width = 2
-
-# Define chat bubble parameters
-bubble_margin = 20
-bubble_radius = 10
-user_bubble_color = (255, 255, 255)
-agent_bubble_color = (0, 0, 255)
-text_color = (0, 0, 0)
-font_size = 20
-font = pygame.font.SysFont(None, font_size)
-
-# Main game loop
+# Loop principal do jogo
 running = True
-is_chat_open = False  # Flag to check if the chatbox is open
 while running:
+    # Verifica eventos
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            # Check if the button is pressed
-            if button_pos[0] <= event.pos[0] <= button_pos[0] + button_width and \
-                    button_pos[1] <= event.pos[1] <= button_pos[1] + button_height:
-                is_chat_open = True
+            if event.button == 1:  # Botão esquerdo do mouse pressionado
+                button_current_scale = BUTTON_PRESSED_SCALE
+        elif event.type == pygame.MOUSEBUTTONUP:
+            if event.button == 1:  # Botão esquerdo do mouse solto
+                button_current_scale = 1.0
 
-    # Check for mouse hover on the button
+    # Atualiza a escala do botão quando o mouse está sobre ele
     mouse_pos = pygame.mouse.get_pos()
-    is_hover = button_pos[0] <= mouse_pos[0] <= button_pos[0] + button_width and \
-               button_pos[1] <= mouse_pos[1] <= button_pos[1] + button_height
-
-    # Scale the button based on hover and press status
-    if is_hover:
-        button_image_scaled = pygame.transform.scale(button_image, button_scale_hover)
-    elif is_chat_open:
-        button_image_scaled = pygame.transform.scale(button_image, button_scale_pressed)
+    button_rect = button_normal_image.get_rect()
+    button_rect.center = (screen_width // 2, screen_height // 2)
+    button_scaled_rect = pygame.Rect(button_rect)
+    button_scaled_rect.width *= button_current_scale
+    button_scaled_rect.height *= button_current_scale
+    if button_scaled_rect.collidepoint(mouse_pos):
+        button_current_scale = BUTTON_HOVER_SCALE
     else:
-        button_image_scaled = pygame.transform.scale(button_image, button_scale_normal)
+        button_current_scale = 1.0
 
-    # Draw the image and button on the screen
-    screen.blit(image, (0, 0))
-    screen.blit(button_image_scaled, button_pos)
+    # Redimensiona a janela se a resolução do monitor mudou
+    if screen_width != screen_info.current_w or screen_height != screen_info.current_h:
+        screen_width = screen_info.current_w
+        screen_height = screen_info.current_h
+        screen = pygame.display.set_mode((screen_width, screen_height), pygame.FULLSCREEN)
 
-    if is_chat_open:
-        # Draw the chatbox
-        pygame.draw.rect(screen, chatbox_border_color, (chatbox_pos[0], chatbox_pos[1], chatbox_width, chatbox_height),
-                         chatbox_border_width)
-        pygame.draw.rect(screen, chatbox_color,
-                         (chatbox_pos[0] + chatbox_border_width, chatbox_pos[1] + chatbox_border_width,
-                          chatbox_width - 2 * chatbox_border_width, chatbox_height - 2 * chatbox_border_width))
+    # Desenha o background
+    background_image_scaled = pygame.transform.scale(background_image, (screen_width, screen_height))
+    screen.blit(background_image_scaled, (0, 0))
 
-        # Draw user question bubble
-        user_question = "Olá! Qual é a sua pergunta?"
-        user_question_surface = font.render(user_question, True, text_color)
-        user_question_width = user_question_surface.get_width()
-        user_question_height = user_question_surface.get_height()
-        user_bubble_pos = (chatbox_pos[0] + bubble_margin, chatbox_pos[1] + bubble_margin)
-        pygame.draw.rect(screen, user_bubble_color,
-                         (user_bubble_pos[0], user_bubble_pos[1], user_question_width + 2 * bubble_margin,
-                          user_question_height + 2 * bubble_margin), bubble_radius)
-        screen.blit(user_question_surface, (user_bubble_pos[0] + bubble_margin, user_bubble_pos[1] + bubble_margin))
+    # Desenha o botão
+    draw_button()
 
-        # Draw agent answer bubble
-        agent_answer = "Aguarde, estou buscando uma resposta..."
-        agent_answer_surface = font.render(agent_answer, True, text_color)
-        agent_answer_width = agent_answer_surface.get_width()
-        agent_answer_height = agent_answer_surface.get_height()
-        agent_bubble_pos = (chatbox_pos[0] + bubble_margin,
-                            user_bubble_pos[1] + bubble_margin + user_question_height + bubble_margin)
-        pygame.draw.rect(screen, agent_bubble_color,
-                         (agent_bubble_pos[0], agent_bubble_pos[1], agent_answer_width + 2 * bubble_margin,
-                          agent_answer_height + 2 * bubble_margin), bubble_radius)
-        screen.blit(agent_answer_surface, (agent_bubble_pos[0] + bubble_margin, agent_bubble_pos[1] + bubble_margin))
-
-        # TODO: Implement API integration to get responses and add them to the chatbox
-
+    # Atualiza a tela
     pygame.display.flip()
 
-# Quit Pygame
+# Encerra o Pygame
 pygame.quit()
